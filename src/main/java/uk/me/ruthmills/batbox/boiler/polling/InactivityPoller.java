ee2d40b6425c29995cf66b580504707c932b5532
Ruth Mills <ruth.mills@connect-group.com>
2016-10-10 20:25:36 +0100

    Take out logging. Add enable scheduling annotation.

--- src/main/java/uk/me/ruthmills/batbox/boiler/polling/InactivityPoller.java
+++ src/main/java/uk/me/ruthmills/batbox/boiler/polling/InactivityPoller.java
@@ -1,6 +1,5 @@
 package uk.me.ruthmills.batbox.boiler.polling;
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -10,14 +9,11 @@ import uk.me.ruthmills.batbox.boiler.service.BoilerService;
 @Component
 public class InactivityPoller {
 	
-	private static final Logger LOGGER = Logger.getLogger(InactivityPoller.class);
-
 	@Autowired
 	private BoilerService boilerService;
 
 	@Scheduled(cron = "*/1 * * * * *")
 	public void checkInactivity() {
-		LOGGER.info("CHECK INACTIVITY");
 		boilerService.checkInactivity();
 	}
 }
